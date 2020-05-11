4ef04ccfbf6dade6f9582922f13f1907341604f5
Chafique <chafique.delli@akretion.com>
2020-05-11 13:34:50 +0200

    [ADD] export_with_record test

--- pattern_import_export/tests/test_pattern_export.py
+++ pattern_import_export/tests/test_pattern_export.py
@@ -64,3 +64,46 @@ class TestPatternExport(SavepointCase):
         self.assertEqual(sheet2.cell_value(1, 0), "BE")
         self.assertEqual(sheet2.cell_value(2, 0), "FR")
         self.assertEqual(sheet2.cell_value(3, 0), "US")
+
+    def test_export_with_record(self):
+        exports_vals = {"name": "My Partner list", "resource": "res.partner"}
+        ir_exports = self.env["ir.exports"].create(exports_vals)
+        field = self.env.ref("base.field_res_country__name")
+        model = self.env.ref("base.model_res_country")
+        select_tab_vals = {
+            "name": "Country list",
+            "model_id": model.id,
+            "field_id": field.id,
+        }
+        select_tab = self.env["ir.exports.select.tab"].create(select_tab_vals)
+        exports_line_vals = [
+            {"name": "id", "export_id": ir_exports.id},
+            {"name": "name", "export_id": ir_exports.id},
+            {"name": "street", "export_id": ir_exports.id},
+            {
+                "name": "country_id",
+                "export_id": ir_exports.id,
+                "select_tab_id": select_tab.id,
+            },
+        ]
+        self.env["ir.exports.line"].create(exports_line_vals)
+        partner_1 = self.env.ref("base.res_partner_1")
+        partner_2 = self.env.ref("base.res_partner_2")
+        partner_3 = self.env.ref("base.res_partner_3")
+        records = [partner_1, partner_2, partner_3]
+        export_file = ir_exports._export_with_record(records)
+        decoded_data = base64.b64decode(export_file)
+        wb = open_workbook(file_contents=decoded_data)
+        sheet1 = wb.sheet_by_index(0)
+        self.assertEqual(sheet1.cell_value(1, 0), 9)
+        self.assertEqual(sheet1.cell_value(2, 0), 10)
+        self.assertEqual(sheet1.cell_value(3, 0), 11)
+        self.assertEqual(sheet1.cell_value(1, 1), "Wood Corner")
+        self.assertEqual(sheet1.cell_value(2, 1), "Deco Addict")
+        self.assertEqual(sheet1.cell_value(3, 1), "Gemini Furniture")
+        self.assertEqual(sheet1.cell_value(1, 2), "1164 Cambridge Drive")
+        self.assertEqual(sheet1.cell_value(2, 2), "325 Elsie Drive")
+        self.assertEqual(sheet1.cell_value(3, 2), "1128 Lunetta Street")
+        self.assertEqual(sheet1.cell_value(1, 3), "United States")
+        self.assertEqual(sheet1.cell_value(2, 3), "United States")
+        self.assertEqual(sheet1.cell_value(3, 3), "United States")
